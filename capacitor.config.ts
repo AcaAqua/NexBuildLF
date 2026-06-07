912fe71551ee9063f2274d6ca39fb73aafcce563
LuckyFields.LLC <clystalblue77@gmail.com>
2026-06-08 02:32:10 +0900

    Improve Android readiness settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
   appId: "com.luckyfields.nexbuildlf",
-  appName: "NexBuildLF",
+  appName: "工程管理 Pro",
   webDir: "out"
 }
 
